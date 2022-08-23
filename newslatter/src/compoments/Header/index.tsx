6b6f2e72805f286c23416b5ce1aab0040b6a8720
luiz <luizes_@hotmail.com>
2022-08-22 21:37:25 -0300

    primeiro commit

---
+++ newslatter/src/compoments/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="newslatter" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+                <SignInButton></SignInButton>
+
+            </div>
+        </header>
+    );
+}
